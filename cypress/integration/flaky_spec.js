7777657919cf3c59f4e9f952ea2b5b1768d93918
Jeff Tucker <jeff@cypress.io>
2020-08-13 10:29:28 -0400

    Try a better random failure rate method
    * This is an extended commit summary to test out SVC-288
    * More lines
    * More words
    * Blah blah blah

--- cypress/integration/flaky_spec.js
+++ cypress/integration/flaky_spec.js
@@ -1,8 +1,8 @@
 describe("Flaky test", function () {
   for (let i = 1; i <= 10; i++) {
     it(`should pass ${i * 10}% of the time`, function () {
-      const subject = Math.round((10 - i) * Math.random());
-      cy.wrap(subject).should("eq", 0);
+      const subject = Math.random();
+      cy.wrap(subject).should("be.lt", i / 10);
     });
   }
 });
