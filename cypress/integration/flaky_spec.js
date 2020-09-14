c77e2e04d761b593f9c065507bafb10821537053
Jeff Tucker <jeff@cypress.io>
2020-09-14 10:56:19 -0400

    Oops

--- cypress/integration/flaky_spec.js
+++ cypress/integration/flaky_spec.js
@@ -2,7 +2,7 @@ describe("Flaky test", function () {
   for (let i = 1; i <= 10; i++) {
     it(`should pass ${i * 10}% of the time`, function () {
       const subject = Math.random();
-      expect(subject).to.eq(i / 10);
+      expect(subject).to.be.lt(i / 10);
     });
   }
 });
