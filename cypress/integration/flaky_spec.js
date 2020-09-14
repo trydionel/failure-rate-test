70950ebe724e8c88a95baf39a159b860d59fb99e
Jeff Tucker <jeff@cypress.io>
2020-09-14 10:54:45 -0400

    Faster failing assertion

--- cypress/integration/flaky_spec.js
+++ cypress/integration/flaky_spec.js
@@ -2,7 +2,7 @@ describe("Flaky test", function () {
   for (let i = 1; i <= 10; i++) {
     it(`should pass ${i * 10}% of the time`, function () {
       const subject = Math.random();
-      cy.wrap(subject).should("be.lt", i / 10);
+      expect(subject).to.eq(i / 10);
     });
   }
 });
