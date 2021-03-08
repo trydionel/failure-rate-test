7eaf5c8ca902e996cc47b4cef1431b8ef9ac8a15
Jeff Tucker <jeff@cypress.io>
2021-03-08 16:08:43 -0500

    whatever

--- cypress/integration/flaky_spec.js
+++ cypress/integration/flaky_spec.js
@@ -1,8 +1,17 @@
 describe("Flaky test", function () {
-  for (let i = 1; i <= 10; i++) {
-    it(`should pass ${i * 10}% of the time`, function () {
-      const subject = Math.random();
-      expect(subject).to.be.lt(i / 10);
+  for (let i = 1; i <= 100; i++) {
+    it(`should pass ${i * 1}% of the time`, function () {
+      const subject = 100 * Math.random();
+      expect(subject).to.be.lt(i / 1);
+    });
+
+    describe("with a really long test name", function () {
+      describe("like no, really really long", function () {
+        it(`should pass ${i * 1}% of the time`, function () {
+          const subject = 100 * Math.random();
+          expect(subject).to.be.lt(i / 1);
+        });
+      });
     });
   }
 });
